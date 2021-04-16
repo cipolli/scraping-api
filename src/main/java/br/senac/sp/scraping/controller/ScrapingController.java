14cb3d9b9d2e8c2be62a69dfe16c530ec8bbf42c
vini <viniciuscipolli@gmail.com>
2021-04-16 18:21:21 -0300

    endpoint  para  scraping

--- src/main/java/br/senac/sp/scraping/controller/ScrapingController.java
+++ src/main/java/br/senac/sp/scraping/controller/ScrapingController.java
@@ -7,8 +7,6 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 
-import javax.websocket.server.PathParam;
-
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -17,16 +15,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/scraping")
 public class ScrapingController {
-	
+
 	@GetMapping
-	public String helloWord(@RequestParam String url) throws IOException {	
+	public String helloWord(@RequestParam String url) throws IOException {
 		StringBuilder result = new StringBuilder();
-		System.out.println("urlToRead");d  
+		System.out.println("urlToRead");
 		System.out.println(url);
 		URL urlObj = new URL(url);
 		HttpURLConnection conn = (HttpURLConnection) urlObj.openConnection();
 		conn.setRequestMethod("GET");
-		try ( BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
+		try (BufferedReader reader = new BufferedReader(
+				new InputStreamReader(conn.getInputStream(), StandardCharsets.UTF_8))) {
 			for (String line; (line = reader.readLine()) != null;) {
 				result.append(line);
 			}
